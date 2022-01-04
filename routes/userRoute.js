84597b9b87a84b08839c725d84c3641adc11f507
Christian Oliveira Leichtweis <chris99leichtweiss@gmail.com>
2022-01-04 10:43:12 -0300

    criando a rota de delete

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -48,7 +48,14 @@ const userRouter = (app) => {
 
             res.status(200).send('OK');
         })
-        
+
+        .delete((req, res) => {
+            const users = getUsers();
+            saveUser(users.filter(user => user.id !== req.params.id));
+
+            res.status(200).send('OK');
+        })
+
 }
 
 module.exports = userRouter;
