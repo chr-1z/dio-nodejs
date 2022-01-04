f052e32fc77012bbce40e3e8f61109d61ab03534
Christian Oliveira Leichtweis <chris99leichtweiss@gmail.com>
2022-01-04 09:51:08 -0300

    criando a rota

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+
+const userRoute = require('./routes/userRoute');
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Olá mundo pelo Express!'));
+userRoute(app);
+
+app.get('/', (req, res) => res.send('Olá mundo!'));
 
 app.listen(port, () => console.log('Api rodando na porta 3000'));
